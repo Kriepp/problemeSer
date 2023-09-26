76b841620935aed715152e869e5ad9a17d037c5f
Kriepp <113834453+Kriepp@users.noreply.github.com>
2023-09-26 19:37:21 +0300

    Add files via upload

--- probleme.js
+++ probleme.js
@@ -1,20 +1,31 @@
-// Functie pentru a vorbi cu pasarile
-// Buna, tu ce parere ai despre sistemul american de supraveghiere?
-// TODO elimina whitespace si punctuatiile din string prima data
+const ending = `pa`;
 
-function vorbiPasare(str) {
-  let rez = [];
-  let lStr = str
+function join(a, b) {
+  if (!a) return ``;
+  if (!b) b = `, `;
+
+  let output = ``;
+  for (let i = 0; i < a.length; i++) {
+    output += i > 0 ? b : ``;
+    output += a[i];
+  }
+  return output;
+}
+
+function talkingToBird(str) {
+  let lowerStr = str
     .replace(/[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/g, '')
     .split(' ')
     .join('')
     .toLowerCase();
-  for (let i = 0; i < lStr.length; i += 2) {
-    if (!lStr[i + 1]) {
-      rez.push(lStr[i] + `pa`);
+
+  let result = [];
+  for (let i = 0; i < lowerStr.length; i += 2) {
+    if (!lowerStr[i + 1]) {
+      result.push(lowerStr[i] + ending);
     } else {
-      rez.push(lStr[i] + lStr[i + 1] + 'pa');
+      result.push(lowerStr[i] + lowerStr[i + 1] + ending);
     }
   }
-  return rez.join(', ');
+  return join(result, `, `);
 }
